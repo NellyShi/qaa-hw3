de6253294bc8fd8a8f11cbd6ec3c70e8cf68ed8b
Rinat Shigapov <r.shigapov@rogii.com>
2024-04-14 18:57:04 +0400

    Fix tests

--- src/test/java/ru/netology/web/ApplicationForCardTest.java
+++ src/test/java/ru/netology/web/ApplicationForCardTest.java
@@ -2,67 +2,75 @@ package ru.netology.web;
 
 import org.junit.jupiter.api.Test;
 
-import static com.codeborne.selenide.Condition.exist;
+import static com.codeborne.selenide.Condition.cssValue;
+import static com.codeborne.selenide.Condition.exactText;
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
 
 class ApplicationForCardTest {
     @Test
+    void validateNamePhoneAndCheckbox1() {
+        open("http://localhost:9999");
+        $("[data-test-id='name'] input").setValue("Шигапова Нэлли");
+        $("[data-test-id='phone'] input").setValue("+79000000000");
+        $("[data-test-id='agreement'] .checkbox__box").click();
+        $("button").click();
+        $("[data-test-id='order-success']").shouldHave(
+                exactText("Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время."));
+    }
+
+    @Test
+    void validateNamePhoneAndCheckbox2() {
+        open("http://localhost:9999");
+        $("[data-test-id='name'] input").setValue("Шигапова-Нэлли");
+        $("[data-test-id='phone'] input").setValue("+79000000009");
+        $("[data-test-id='agreement'] .checkbox__box").click();
+        $("button").click();
+        $("[data-test-id='order-success']").shouldHave(
+                exactText("Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время."));
+    }
+
+    @Test
     void shouldNotNameBeLatin() {
         open("http://localhost:9999");
-        $("[data-test-id=name] input").setValue("Shigapova Nelli");
-        $("[data-test-id=phone] input").setValue("+79000000000");
-        $("[data-test-id=agreement] .checkbox__box").click();
+        $("[data-test-id='name'] input").setValue("Shigapova Nelli");
+        $("[data-test-id='phone'] input").setValue("+79000000000");
+        $("[data-test-id='agreement'] .checkbox__box").click();
         $("button").click();
-        $(".Success_successBlock__2L3Cw").shouldNot(exist);
+        $("[data-test-id='name'].input_invalid .input__sub").shouldHave(
+                exactText("Имя и Фамилия указаные неверно. Допустимы только русские буквы, пробелы и дефисы."));
     }
 
     @Test
     void shouldNotNameContainsUnderscore() {
         open("http://localhost:9999");
-        $("[data-test-id=name] input").setValue("Шигапова_Нэлли");
-        $("[data-test-id=phone] input").setValue("+79000000000");
-        $("[data-test-id=agreement] .checkbox__box").click();
+        $("[data-test-id='name'] input").setValue("Шигапова_Нэлли");
+        $("[data-test-id='phone'] input").setValue("+79000000000");
+        $("[data-test-id='agreement'] .checkbox__box").click();
         $("button").click();
-        $(".Success_successBlock__2L3Cw").shouldNot(exist);
-    }
-
-    @Test
-    void shouldNameBeCyrillicAndContainsSpace() {
-        open("http://localhost:9999");
-        $("[data-test-id=name] input").setValue("Шигапова    Нэлли");
-        $("[data-test-id=phone] input").setValue("+79000000000");
-        $("[data-test-id=agreement] .checkbox__box").click();
-        $("button").click();
-        $(".Success_successBlock__2L3Cw").should(exist);
+        $("[data-test-id='name'].input_invalid .input__sub").shouldHave(
+                exactText("Имя и Фамилия указаные неверно. Допустимы только русские буквы, пробелы и дефисы."));
     }
 
     @Test
     void shouldNotPhoneBeMoreThen11() {
         open("http://localhost:9999");
-        $("[data-test-id=name] input").setValue("Шигапова    Нэлли");
-        $("[data-test-id=phone] input").setValue("+7000000000000");
-        $("[data-test-id=agreement] .checkbox__box").click();
+        $("[data-test-id='name'] input").setValue("Шигапова Нэлли");
+        $("[data-test-id='phone'] input").setValue("+7000000000000");
+        $("[data-test-id='agreement'] .checkbox__box").click();
         $("button").click();
-        $(".Success_successBlock__2L3Cw").shouldNot(exist);
+        $("[data-test-id='phone'].input_invalid .input__sub").shouldHave(
+                exactText("Телефон указан неверно. Должно быть 11 цифр, например, +79012345678."));
     }
 
     @Test
-    void shouldFirstCharPhoneBeOnlyPlus() {
+    void shouldNotFirstCharPhoneBeNotPlus() {
         open("http://localhost:9999");
-        $("[data-test-id=name] input").setValue("Шигапова    Нэлли");
-        $("[data-test-id=phone] input").setValue("79000000000");
-        $("[data-test-id=agreement] .checkbox__box").click();
+        $("[data-test-id='name'] input").setValue("Шигапова    Нэлли");
+        $("[data-test-id='phone'] input").setValue("79000000000");
+        $("[data-test-id='agreement'] .checkbox__box").click();
         $("button").click();
-        $(".Success_successBlock__2L3Cw").shouldNot(exist);
-    }
-
-    @Test
-    void shouldBeAgreementCheckBoxSet() {
-        open("http://localhost:9999");
-        $("[data-test-id=name] input").setValue("Шигапова    Нэлли");
-        $("[data-test-id=phone] input").setValue("+79000000000");
-        $("button").click();
-        $(".Success_successBlock__2L3Cw").shouldNot(exist);
+        $("[data-test-id='phone'].input_invalid .input__sub").shouldHave(
+                exactText("Телефон указан неверно. Должно быть 11 цифр, например, +79012345678."));
     }
 }
